9825c668f36ac9034f01447353a8f3dc34fcda83
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-01 07:36:51 +0200

    create separate file for player script

---
+++ js/player.js
@@ -0,0 +1,8 @@
+//gestisce il riempimento della barra di imput-range
+const rangeInput = document.getElementById("progress-bar");
+
+rangeInput.addEventListener("input", function () {
+  const value = this.value;
+
+  this.style.background = `linear-gradient(to right, #1ed760 ${value}%, #404040 ${value}%)`;
+});
