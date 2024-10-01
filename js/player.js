e18f3c08b945d2920f70733c19c885be9b040cac
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-01 09:19:14 +0200

    add localstorage variable and adjusted index styles

--- js/player.js
+++ js/player.js
@@ -3,6 +3,8 @@ const rangeInput = document.getElementById("progress-bar");
 
 rangeInput.addEventListener("input", function () {
   const value = this.value;
-
   this.style.background = `linear-gradient(to right, #1ed760 ${value}%, #404040 ${value}%)`;
 });
+
+// audio.src = firstTrackPreview;
+// audio.load()
