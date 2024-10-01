ea02958dc340f6472412b714e8f74b0f2f9ab8c8
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-01 07:41:55 +0200

    adds comments to index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
+// endpoint di ricerca
 const searchUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+// artisti predefiniti per aggiornare la pagina al caricamento )
 const previewArray = ["radiohead", "led zeppelin", "beatles", "jamiroquay", "queen"];
 
 const indexPreview = () => {
+  // ciclo l'array di artisti per la preview
   for (let i = 0; i < previewArray.length; i++) {
+    // concateno la url e il nome dell'artista
     fetch(searchUrl + previewArray[i])
       .then((response) => {
         if (response.ok) {
@@ -12,7 +16,9 @@ const indexPreview = () => {
         }
       })
       .then((data) => {
+        // chiamo la funzione per la generazione delle card degli album (sezione "altro di ciò che ti piace")
         generateAlbumCards(data.data);
+        // chiamo la funzione per la generazione delle card degli album (sezione "i tuoi artisti preferiti")
         generateArtistCards(data.data);
       })
       .catch((err) => {
@@ -21,13 +27,17 @@ const indexPreview = () => {
   }
 };
 
-const heroSection = () => {};
+// const heroSection = () => {}; // TODO
 
 const generateAlbumCards = (data) => {
+  // genero randomicamente un indice tra 0 e 4 (serve per mostrare in ordine sempre diverso le card nella pagina)
   const index = Math.floor(Math.random() * 5);
+  // seleziono l'album e l'artista in base all'indice generato
   const artist = data[index].artist;
   const album = data[index].album;
+  // seleziono l'elemento HTML dove inserire le card
   const albums = document.querySelector(".albums");
+  // creo la card
   const cardWrapper = document.createElement("div");
   cardWrapper.classList.add("card-wrapper");
   cardWrapper.innerHTML = `
@@ -38,13 +48,17 @@ const generateAlbumCards = (data) => {
           <a href="./artist.html?artistID=${artist.id}"><p class="playlist-artist">${artist.name}</p></a>
       </div>
     `;
+  // aggiungo la card creata all'HTML
   albums.appendChild(cardWrapper);
 };
 
 const generateArtistCards = (data) => {
+  // genero randomicamente un indice tra 0 e 4 (serve per mostrare in ordine sempre diverso le card nella pagina)
   const index = Math.floor(Math.random() * 5);
+  // seleziono l'artista in base all'indice generato
   const artist = data[index].artist;
   const artists = document.querySelector(".artists");
+  // genero la card
   const cardWrapper = document.createElement("div");
   cardWrapper.classList.add("card-wrapper");
   cardWrapper.innerHTML = `
@@ -54,16 +68,8 @@ const generateArtistCards = (data) => {
           <a href="./artist.html?artistID=${artist.id}"><h6 class="playlist-title" title="${artist.name}">${artist.name}</h6></a>
       </div>
     `;
+  // aggiungo la card all'HTML
   artists.appendChild(cardWrapper);
 };
 
 indexPreview();
-
-//gestisce il riempimento della barra di imput-range
-const rangeInput = document.getElementById("progress-bar");
-
-rangeInput.addEventListener("input", function () {
-  const value = this.value;
-
-  this.style.background = `linear-gradient(to right, #1ed760 ${value}%, #404040 ${value}%)`;
-});
