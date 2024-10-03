205392c49e45f07dd6d45c05a41e26ef730a1d7b
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-03 12:21:15 +0200

    defined page layout and added search field

--- js/search.js
+++ js/search.js
@@ -1,4 +1,5 @@
-const searchUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
+const searchUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=francesco renga";
+const artistTracklist = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
 
 const getSearchResult = () => {
   fetch(searchUrl)
@@ -10,8 +11,25 @@ const getSearchResult = () => {
       }
     })
     .then((searchObj) => {
-      console.log(searchObj.data);
       getSearch(searchObj.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getArtistTopTracks = (artistID) => {
+  fetch(artistTracklist + artistID + "/top?limit=10")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+    .then((topTracks) => {
+      console.log(topTracks.data);
+      showTopTracks(topTracks.data);
     })
     .catch((err) => {
       console.log(err);
@@ -22,9 +40,35 @@ const getSearch = (searchResult) => {
   const image = searchResult[0].artist.picture;
   const name = searchResult[0].artist.name;
   const artistName = document.querySelector(".results h4");
-  artistName.innerHTML = name;
+  artistName.innerHTML = "The Best of " + name;
   const artistImg = document.querySelector(".results img");
   artistImg.src = image;
+  const artistID = searchResult[0].artist.id;
+  getArtistTopTracks(artistID);
+};
+
+const showTopTracks = (topTracks) => {
+  const tracksContainer = document.querySelector(".tracks");
+  for (let i = 0; i < 4; i++) {
+    const singleTrack = topTracks[i];
+    const trackDiv = document.createElement("div");
+    trackDiv.classList.add("single-track");
+    const seconds = singleTrack.duration % 60;
+    if (seconds < 10) {
+      seconds = "0" + seconds.toString();
+    }
+    trackDiv.innerHTML = `
+    <img src="${singleTrack.album.cover}" alt="${singleTrack.album.title}" />
+    <div>
+        <h5>${singleTrack.album.title}</h5>
+        <p>${singleTrack.artist.name}</p>
+    </div>
+    <div>
+        <p>${Math.trunc(singleTrack.duration / 60)}:${seconds}</p>
+    </div>
+    `;
+    tracksContainer.appendChild(trackDiv);
+  }
 };
 
 getSearchResult();
